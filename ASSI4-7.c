0cc4542eb9e8358829aa8c31679a49c414af2199
Tushar3888 <TUSHAR.30008@stu.upes.ac.in>
2026-08-30 15:40:40 +0530

    Add initial implementation of ASSI4-7.c

---
+++ ASSI4-7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    a=10;
+    printf("The initial value of a is %d",a);
+    printf("Using ++a = %d\n", ++a);
+    printf("Value of a after ++a = %d\n", a);
+    a = 10;
+    printf("\nInitial value of a = %d\n", a);
+    printf("Using a++ = %d\n", a++);
+    printf("Value of a after a++ = %d\n", a);
+    a = 10;
+    printf("\nInitial value of a = %d\n", a);
+    printf("Using--a = %d\n",--a); 
+    printf("Value of a after--a = %d\n", a);
+a = 10;
+printf("\nInitial value of a = %d\n", a);
+printf("Using a-- = %d\n", a--);
+printf("Value of a after a-- = %d\n", a);
+return 0;
+}
